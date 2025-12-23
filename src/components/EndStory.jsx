f822d6e7c16c526a6c08009776e9fc60e6c51862
madortill <mador.till@gmail.com>
2025-12-23 18:35:04 +0200

    fix base bath

--- src/components/EndStory.jsx
+++ src/components/EndStory.jsx
@@ -8,7 +8,7 @@ function EndStory({setPage}) {
   return (
     <div className='endStory'>
       <div className='endStory-text'>
-      המפקד שלנו יכול להגיד ביי לרבעו”ש הפעם, אבל נוכל לקוות שהוא וגם אנחנו <span>הפקנו לקחים </span> מהסיפור הזה ובפעם הבאה נוכל לזכות ברבעו”ש המיוחל!
+      המפקד שלנו יכול להגיד ביי לרבעו”ש הפעם, אבל נוכל לקוות שהוא וגם אנחנו <span className='endStory-bold'>הפקנו לקחים </span> מהסיפור הזה ובפעם הבאה נוכל לזכות ברבעו”ש המיוחל!
       </div>
       <button className='startBtn endStoryBtn' onClick={nextPage}>עכשיו תורכם!</button>
     </div>
